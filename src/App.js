1e7b4a975ec6fecb7cc148431ea774a68ec41eff
Saddam <asaddam600@gmail.com>
2023-07-16 12:55:46 +0530

    evn file setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import "./App.css";
-
+import { fetchDataFromApi } from "./utils/api";
 function App() {
+  const apiTestting = () => {
+    fetchDataFromApi("/movie/popular").then((response) => {
+      console.log(response.results);
+    });
+  };
+  useEffect(() => {
+    apiTestting();
+  }, []);
   return <div className="App">App page !! </div>;
 }
 
