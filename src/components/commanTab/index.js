001bb81e904bb2f97ee3b4629168ad8c7e977ead
Saddam <asaddam600@gmail.com>
2023-07-19 18:09:48 +0530

    add react slick

--- src/components/commanTab/index.js
+++ src/components/commanTab/index.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-const CommanTab = () => {
+const CommanTab = ({ data, onTabChange }) => {
+  const [activeTab, setActive] = useState();
+  const [left, setLeft] = useState(0);
+
+  const activeTabHandler = (tab, index) => {
+    setTimeout(() => {
+      setLeft(100 * index);
+      setActive(index);
+    }, 100);
+    onTabChange(tab, index);
+  };
+
   return (
     <div className="comman__tab">
-      <div className="comman__tab__item">Tab One</div>
-      <div className="comman__tab__item">Tab Two</div>
+      <div className="comman__tab__content">
+        <ul className="comman__tab__items">
+          {data.map((tab, index) => (
+            <li
+              key={tab}
+              className={`comman__tab__item ${activeTab === index ? "active" : ""}`}
+              onClick={() => {
+                activeTabHandler(tab, index);
+              }}>
+              {tab}
+            </li>
+          ))}
+        </ul>
+        <div className="tab__slider" style={{ left }}></div>
+      </div>
     </div>
   );
 };
--- src/pages/home/Trending.js
+++ src/pages/home/Trending.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CommanTab from "../../components/commanTab";
+import useFetch from "../../hooks/useFetch";
 
 const Trending = () => {
+  const [endPoint, setEndPoint] = useState("day");
+  const { data, isLoading, error } = useFetch(`/trending/all/${endPoint}`);
+  const onTabChangeHandler = (currantTab, index) => {
+    console.log(currantTab, index);
+    setEndPoint(currantTab);
+  };
+
+  console.log(data);
+
   return (
     <div className="trending">
       <div className="container">
-        <div className="comman-header-section">
-          <h2>Trending</h2>
-          <CommanTab />
+        <div className="slider__title__section">
+          <h2 className="slider__title">Trending</h2>
+          <CommanTab data={["day", "week"]} onTabChange={onTabChangeHandler} />
         </div>
+
+        <div className="tabone">Tab one data</div>
+        <div className="tabone">Tab two data</div>
       </div>
     </div>
   );
